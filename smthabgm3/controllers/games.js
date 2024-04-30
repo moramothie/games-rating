c679a90c7e13b516054e02d74982e021cc6d7cfd
Mora <b.mityanov@ya.ru>
2024-04-30 15:21:27 +0300

    3done

---
+++ smthabgm3/controllers/games.js
@@ -0,0 +1,12 @@
+const sendAllGames = async (req, res) => {
+  res.send(req.games);
+};
+
+const sendUpdatedGames = async (req, res) => {
+  res.send({
+    games: req.games,
+    updated: req.updatedObject
+  });
+};
+
+module.exports = { sendAllGames, sendUpdatedGames}
